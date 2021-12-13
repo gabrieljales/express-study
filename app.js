2406a8510dd2491325b7aa983f20695f1913ccb9
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2021-12-13 17:05:11 -0300

    refactoring: - Parsing middlewares must be declared before routes

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ require('./src/database/index');
 const express = require('express');
 const app = express();
 
-// Rotas
-const indexRouter = require('./src/routes/index');
-app.use('/', indexRouter);
-
-// Middlewares
+// Middlewares -- OBS: Declarar isso antes das rotas!
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Rotas
+const indexRouter = require('./src/routes/index');
+app.use('/', indexRouter);
+const userRouter = require('./src/routes/user');
+app.use('/users/', userRouter);
 
 module.exports = app;
