830b193fd6fda2881cb492291423a8ac8010fc4b
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2021-12-13 20:43:12 -0300

    All users' CRUD routes are done

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 const userController = require('../controllers/user');
 
 // Prefixo das rotas: /users/
-router.post('/', userController.create);
+router.post('/', userController.create); // --> Cria um usuário
+router.get('/', userController.list); // --> Lista todos os usuários
+router.get('/:id', userController.show); // --> Mostra os detalhes de um usuário
+router.put('/:id', userController.update); // --> Atualiza um usuário
+router.delete('/:id', userController.delete); // --> Deleta um usuário
 
 module.exports = router;
