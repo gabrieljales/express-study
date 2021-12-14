398661b5e345a0778dae08520ce83d5e0b8bd375
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2021-12-14 20:25:23 -0300

    Verification: Checking if the user is logged in

--- src/middlewares/loginRequired.js
+++ src/middlewares/loginRequired.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization) {
     return res.status(401).json({
@@ -11,11 +12,27 @@ module.exports = (req, res, next) => {
   const [, token] = authorization.split(' '); // Vai vir: "Bearer(espaço)Token".  por isso o split no token
 
 try {
-  const dados = jwt.verify(token, process.env.SECRET_TOKEN);
-  const { id, email } = dados;
+  const dados = jwt.verify(token, process.env.SECRET_TOKEN); // Dados no payload do token
+  const { id, email } = dados; // Extraindo id e email que vieram no payload
+
+  // Verificando é o mesmo usuário (caso tenha atualizado o email, o token não será mais válido)
+  const user = await User.findOne({
+    where: {
+      id, // É como se fosse id: id, email: email (mas não precisa repetir)
+      email,
+    },
+  });
+
+  if (!user) { // Se o usuário atualizou seu EMAIL, ele terá que realizar login novamente
+    return res.status(401).json({
+      errors: ['Usuário inválido']
+    });
+  }
+
+  // id e email do usuário agora estão disponíveis e são conhecidos no sistema
   req.userId = id;
   req.userEmail = email;
-  return next();
+  return next(); // Continua para a response
 
 } catch (err) {
   return res.status(401).json({
