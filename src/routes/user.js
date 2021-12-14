fd9f3fba5bad28277a4c0bea3f8d7359ee6b99d1
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2021-12-14 20:21:56 -0300

    list and show routes commented

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,11 +5,11 @@ const loginRequired = require('../middlewares/loginRequired');
 // Prefixo das rotas: /users/
 
 // Essas duas rotas só fazem sentido para um Super Admin ou algo assim
-router.get('/', loginRequired, userController.list); // --> Lista todos os usuários
-router.get('/:id', userController.show); // --> Mostra os detalhes de um usuário
+//router.get('/', userController.list); // --> Lista todos os usuários
+//router.get('/:id', userController.show); // --> Mostra os detalhes de um usuário
 
-router.post('/', userController.create); // --> Cria um usuário
-router.put('/:id', userController.update); // --> Atualiza um usuário
-router.delete('/:id', userController.delete); // --> Deleta um usuário
+router.post('/', userController.create); // --> Cria um usuário (aberto para todos)
+router.put('/', loginRequired, userController.update); // --> Atualiza um usuário
+router.delete('/', loginRequired, userController.delete); // --> Deleta um usuário
 
 module.exports = router;
