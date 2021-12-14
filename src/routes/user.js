65106ecc1d56f8d5205008a9c2037c215924b7b6
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2021-12-14 18:04:22 -0300

    Using loginRequired middleware in user routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 const userController = require('../controllers/user');
+const loginRequired = require('../middlewares/loginRequired');
 
 // Prefixo das rotas: /users/
-router.post('/', userController.create); // --> Cria um usuário
-router.get('/', userController.list); // --> Lista todos os usuários
+
+// Essas duas rotas só fazem sentido para um Super Admin ou algo assim
+router.get('/', loginRequired, userController.list); // --> Lista todos os usuários
 router.get('/:id', userController.show); // --> Mostra os detalhes de um usuário
+
+router.post('/', userController.create); // --> Cria um usuário
 router.put('/:id', userController.update); // --> Atualiza um usuário
 router.delete('/:id', userController.delete); // --> Deleta um usuário
 
